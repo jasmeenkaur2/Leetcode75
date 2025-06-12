8114a60744167d786b8026ce4e0588944d792fd9
Jasmeen Kaur <jasmeenrandhawa345@gmail.com>
2025-06-12 07:33:13 +0530

    leetcode75

---
+++ 1.Arrays/Greatest_common_div_string.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <numeric> // Needed for std::gcd
+
+using namespace std;
+
+class Solution
+{
+public:
+    string gcdOfStrings(string str1, string str2)
+    {
+        if (str1 + str2 != str2 + str1)
+        {
+            return "";
+        }
+        int len = std::gcd(str1.length(), str2.length());
+        return str1.substr(0, len);
+    }
+};
